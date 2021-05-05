7dc32e26e18de21bd70a7dafc85494f19015ad32
GabrielLacerda2 <galacerda260401@gmail.com>
2021-05-05 18:39:25 -0300

    commiti inicial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'daaa2cd198c1609fc15052d40472f076'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bab14bc0-5faf-4ff5-b736-78ecd37a5e4c'
+};
